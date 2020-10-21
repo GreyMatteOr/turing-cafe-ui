a8e70496a97b4a301d40297f54fced4e9b985216
Matthew Lane <themplane@gmail.com>
2020-10-21 09:57:25 -0700

    Added input validation

--- src/ResoForm/ResoForm.js
+++ src/ResoForm/ResoForm.js
@@ -13,9 +13,28 @@ class ResoForm extends Component {
         number: 0
       },
       postError: false,
-      reqError: false
+      reqError: false,
+      readyToSubmit: false
     };
-    update: this.props.getResos;
+    updateResos: this.props.getResos;
+  }
+
+  updateBody(bodyProp, value) {
+    if(bodyProp === 'date') {
+      let [year, month, day] = value.split('-');
+      value = `${month}/${day}`;
+    }
+    let resBody = this.state.resBody;
+    update[bodyProp] = value;
+    let hasName = update.name !== '';
+    let isAfterToday = this.isAfter(update.date);
+    let readyToSubmit = hasName && isAfterToday
+    this.setState({resBody, readyToSubmit});
+  }
+
+  isAfterToday(date) {
+    let [dateMonth, dateDay] = date.date.split('/');
+    return (+dateMonth - 10 || +dateDay - 22) > 0
   }
 
   render() {
@@ -23,24 +42,35 @@ class ResoForm extends Component {
       <>
         <h3 className='newResoFormTitle'>Make a Reservation</h3>
         <input
+          type='text'
           className='newResoName'
           value={this.state.resBody.name}
           onChange={event => this.updateBody('name', event.target.value)}
           placeholder='What is the name for the reservation?'
         />
         <input
+          type='date'
+          min='2020-01-01'
+          max='2020-12-31'
           className='newResoDate'
           value={this.state.resBody.date}
           onChange={event => this.updateBody('date', event.target.value)}
           placeholder='What date would you like to stop in?'
         />
         <input
+          type='time'
+          min='12:00'
+          max='22:00'
+          step='900'
           className='newResoTime'
           value={this.state.resBody.time}
           onChange={event => this.updateBody('time', event.target.value)}
           placeholder='What time will you join us?'
         />
         <input
+          type='number'
+          min='0'
+          max='30'
           className='newResoGuests'
           value={this.state.resBody.number}
           onChange={event => this.updateBody('number', event.target.value)}
